feba9d81800cc2a1db1552fb9aab11b2dfc082cf
smorgannicole <morgann.smithh@yahoo.com>
2024-05-30 08:09:17 -0700

    fxn to parse str to num

--- index.ts
+++ index.ts
@@ -6,8 +6,12 @@ const main = (): void => {
   const firstStr: string = question("Enter first number:\n");
   const operator: string = question("Enter operator:\n");
   const secondStr: string = question("Enter second number:\n");
+};
 
-  console.log(firstStr, operator, secondStr);
+const isItNum = (str: string): boolean => {
+  const maybeNum = parseFloat(str); // will either return the num ie 44 or NaN
+  const isNum: boolean = !isNaN(maybeNum); // should return T if ^ is actually a num in a str
+  return isNum;
 };
 
 main();
