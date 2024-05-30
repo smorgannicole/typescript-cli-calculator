9f439162f1555a0afb8b4ee5a5ce7faf2f9dd8f5
smorgannicole <morgann.smithh@yahoo.com>
2024-05-30 07:57:29 -0700

    inputs received

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = require("readline-sync");
+// question always returns a string
+// question doesnt create new line by default
+var main = function () {
+    var firstStr = (0, readline_sync_1.question)("Enter first number:\n");
+    var operator = (0, readline_sync_1.question)("Enter operator:\n");
+    var secondStr = (0, readline_sync_1.question)("Enter second number:\n");
+    console.log(firstStr, operator, secondStr);
+};
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("test");
+import { question } from "readline-sync";
+// question always returns a string
+// question doesnt create new line by default
+
+const main = (): void => {
+  const firstStr: string = question("Enter first number:\n");
+  const operator: string = question("Enter operator:\n");
+  const secondStr: string = question("Enter second number:\n");
+
+  console.log(firstStr, operator, secondStr);
+};
+
+main();
