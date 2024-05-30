7d79af74499bd1566ab7863971351d15ef838738
smorgannicole <morgann.smithh@yahoo.com>
2024-05-30 08:19:25 -0700

    fxn to validate operator

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ var readline_sync_1 = require("readline-sync");
 // question always returns a string
 // question doesnt create new line by default
 var main = function () {
-    var firstStr = (0, readline_sync_1.question)("Enter first number:\n");
-    var operator = (0, readline_sync_1.question)("Enter operator:\n");
-    var secondStr = (0, readline_sync_1.question)("Enter second number:\n");
-    console.log(firstStr, operator, secondStr);
+    var firstNumStr = (0, readline_sync_1.question)("Enter first number:\n");
+    var operatorStr = (0, readline_sync_1.question)("Enter operator:\n");
+    var secondNumStr = (0, readline_sync_1.question)("Enter second number:\n");
+    var operator = isItOperator(operatorStr);
+    console.log(operator);
+};
+var isItNum = function (str) {
+    var maybeNum = parseFloat(str); // will either return the num ie 44 or NaN
+    var isNum = !isNaN(maybeNum); // should return T if ^ is actually a num in a str
+    return isNum;
+};
+var isItOperator = function (str) {
+    switch (str) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            return true;
+        default:
+            return false;
+    }
 };
 main();
--- index.ts
+++ index.ts
@@ -3,15 +3,30 @@ import { question } from "readline-sync";
 // question doesnt create new line by default
 
 const main = (): void => {
-  const firstStr: string = question("Enter first number:\n");
-  const operator: string = question("Enter operator:\n");
-  const secondStr: string = question("Enter second number:\n");
+  const firstNumStr: string = question("Enter first number:\n");
+  const operatorStr: string = question("Enter operator:\n");
+  const secondNumStr: string = question("Enter second number:\n");
+
+  const operator = isItOperator(operatorStr);
+  console.log(operator);
 };
 
 const isItNum = (str: string): boolean => {
   const maybeNum = parseFloat(str); // will either return the num ie 44 or NaN
   const isNum: boolean = !isNaN(maybeNum); // should return T if ^ is actually a num in a str
   return isNum;
+};
+
+const isItOperator = (str: string): boolean => {
+  switch (str) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      return true;
+    default:
+      return false;
+  }
 };
 
 main();
