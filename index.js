dd99fb0edfd03ce16c7db5a95063876720247aa5
smorgannicole <morgann.smithh@yahoo.com>
2024-05-30 08:48:09 -0700

    simple cli complete

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var readline_sync_1 = require("readline-sync");
-// question always returns a string
-// question doesnt create new line by default
 var main = function () {
     var firstNumStr = (0, readline_sync_1.question)("Enter first number:\n");
     var operatorStr = (0, readline_sync_1.question)("Enter operator:\n");
     var secondNumStr = (0, readline_sync_1.question)("Enter second number:\n");
-    var operator = isItOperator(operatorStr);
-    console.log(operator);
+    var isInputValid = isItNum(firstNumStr) && isItOperator(operatorStr) && isItNum(secondNumStr);
+    if (isInputValid) {
+        var firstNum = parseFloat(firstNumStr);
+        var secondNum = parseFloat(secondNumStr);
+        var result = calculate(firstNum, operatorStr, secondNum);
+        console.log("\nResult: ".concat(result, "\n"));
+    }
+    else {
+        console.log("\nInvalid input. Try again :-)\n");
+        main();
+    }
 };
 var isItNum = function (str) {
     var maybeNum = parseFloat(str); // will either return the num ie 44 or NaN
@@ -24,6 +31,20 @@ var isItOperator = function (str) {
             return true;
         default:
             return false;
+    }
+};
+var calculate = function (num, op, num2) {
+    switch (op) {
+        case "+":
+            return num + num2;
+        case "-":
+            return num - num2;
+        case "*":
+            return num * num2;
+        case "/":
+            return num / num2;
+        default:
+            throw new Error("Invalid operator");
     }
 };
 main();
--- index.ts
+++ index.ts
@@ -2,13 +2,29 @@ import { question } from "readline-sync";
 // question always returns a string
 // question doesnt create new line by default
 
+type Operator = "+" | "-" | "*" | "/";
+
 const main = (): void => {
   const firstNumStr: string = question("Enter first number:\n");
   const operatorStr: string = question("Enter operator:\n");
   const secondNumStr: string = question("Enter second number:\n");
 
-  const operator = isItOperator(operatorStr);
-  console.log(operator);
+  const isInputValid: boolean =
+    isItNum(firstNumStr) && isItOperator(operatorStr) && isItNum(secondNumStr);
+
+  if (isInputValid) {
+    const firstNum: number = parseFloat(firstNumStr);
+    const secondNum: number = parseFloat(secondNumStr);
+    const result: number = calculate(
+      firstNum,
+      operatorStr as Operator,
+      secondNum
+    );
+    console.log(`\nResult: ${result}\n`);
+  } else {
+    console.log("\nInvalid input. Try again :-)\n");
+    main();
+  }
 };
 
 const isItNum = (str: string): boolean => {
@@ -26,6 +42,21 @@ const isItOperator = (str: string): boolean => {
       return true;
     default:
       return false;
+  }
+};
+
+const calculate = (num: number, op: Operator, num2: number): number => {
+  switch (op) {
+    case "+":
+      return num + num2;
+    case "-":
+      return num - num2;
+    case "*":
+      return num * num2;
+    case "/":
+      return num / num2;
+    default:
+      throw new Error("Invalid operator");
   }
 };
 
